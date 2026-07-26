c2b7f8902787e382e5b8732776a23c612099962d
Gamedagamez <gamedagames@gmail.com>
2026-07-26 02:00:26 -0600

    adjust audio

--- Scenes/UI/PauseMenu/pause_menu.gd
+++ Scenes/UI/PauseMenu/pause_menu.gd
@@ -3,6 +3,9 @@ extends Control
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
 
+func _ready() -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("music"),(get_node("SensContainer2/music_slider").value - 100)/3)
+
 func _on_main_menu_button_pressed() -> void:
 	toggle_pause()
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
